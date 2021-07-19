973492cdf56024ace28bd818ab5f6deb6a02d8e0
Dianne <mdianne.gabriel@gmail.com>
2021-07-19 15:53:06 -0400

    Practice js head function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(head, expected) {
+
+  if (head === expected) {
+    console.log(`✅ Assertion Passed: ${head} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${head} !== ${expected}`);
+  }
+
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
