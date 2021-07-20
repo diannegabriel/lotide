747228ebcd2b4a7bebd8221efcf346f75b1ca970
Dianne <mdianne.gabriel@gmail.com>
2021-07-20 16:34:09 -0400

    Updated assertEqual, used ternary

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,19 @@
+/*
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
+}; 
+*/
+
+const assertEqual = function(actual, expected) {
+  return actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}`
+  : `🛑 Assertion Failed: ${actual} !== ${expected}`
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
