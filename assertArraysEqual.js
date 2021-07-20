2fc726316e32448b297b1f4a97fd9a35cf702b1a
Dianne <mdianne.gabriel@gmail.com>
2021-07-20 18:07:36 -0400

    Created and programmed assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,18 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   return Array.isArray(array1) && 
     Array.isArray(array2) &&
     array1.length === array2.length &&
     array1.every((val, index) => val === array2[index]);
 };
 
-const assertEqual = function(boolean) {
-  boolean === true ? console.log(`✅ Assertion Passed!`)
-  : console.log(`🛑 Assertion Failed.`)
+const assertArraysEqual = function(array1, array2) {
+  (eqArrays(array1, array2) === true) ? 
+  console.log(`✅ Assertion Passed! ${array1} === ${array2}`)
+  : console.log(`🛑 Assertion Failed. ${array1} !== ${array2}`)
+}; 
 
-};
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
-assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
-
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
