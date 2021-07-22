e976c1786e1f6d17b2d5fbc63e7b3ded84756599
Dianne <mdianne.gabriel@gmail.com>
2021-07-21 20:19:25 -0400

    Created and programmed countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -23,4 +23,5 @@ const countLetters = function(string) {
   return strCount;
 };
 
-assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 })
+assertEqual(countLetters("lighthouse in the house")['l'], [1])
+assertEqual(countLetters("abc")['a'], 1)
