8e60fb7a86b639b2ce8452d2cd9ad3362079493f
Dianne <mdianne.gabriel@gmail.com>
2021-07-26 11:50:54 -0400

    Updated. Refactored. Added assertion exports

--- middle.js
+++ middle.js
@@ -1,35 +1,3 @@
-const eqArrays = function(array1, array2) {
-  // compare two arrays together, return true if both arrays match, otherwise return false
-
-  // compare each element of two arrays, because arrays are structural type
-
-  // to compare the arrays, must compare the value at the same index
-
-  // to access all of the indexes, loop over the values
-  if (array2.length !== array1.length) {
-    return false
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    // return false the soonest the loop finds a mismatch 
-    if (array1[i] !== array2[i]) {
-      return false
-    } 
-  }
-  return true
-  // return true when everything matches until the loop ends
-
-};
-
-const assertArraysEquals = function(array1, array2) {
-// I assert that these two arrays are Equal (array1, array2)
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed!`)
-  } else {
-    console.log(`🛑 Assertion Failed.`)
-  }
-};
-
 const middle = function(array) {
   let midIndex = Math.floor(array.length/2);
   let middleArray = [];
@@ -45,12 +13,4 @@ const middle = function(array) {
   }
 }
 
-assertArraysEquals(middle([1]), []) // => []
-assertArraysEquals(middle([1, 2]), []) // => []
-
-assertArraysEquals(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEquals(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-
-assertArraysEquals(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEquals(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
-assertArraysEquals(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5,6])
+module.exports = middle;
